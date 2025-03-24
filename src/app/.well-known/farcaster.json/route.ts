885b823976d3bcb2f00a99153742b73bd83f6835
sahban <sahbaan@proton.me>
2025-03-24 19:18:45 +0530

    add mint and many more things

--- src/app/.well-known/farcaster.json/route.ts
+++ src/app/.well-known/farcaster.json/route.ts
@@ -1,39 +1,16 @@
 export async function GET() {
   const appUrl = process.env.NEXT_PUBLIC_URL;
 
-  const config = 
-// {
-//     accountAssociation: {
-//       header:
-//         "eyJmaWQiOjM2MjEsInR5cGUiOiJjdXN0b2R5Iiwia2V5IjoiMHgyY2Q4NWEwOTMyNjFmNTkyNzA4MDRBNkVBNjk3Q2VBNENlQkVjYWZFIn0",
-//       payload: "eyJkb21haW4iOiJmcmFtZXMtdjIudmVyY2VsLmFwcCJ9",
-//       signature:
-//         "MHhiNDIwMzQ1MGZkNzgzYTExZjRiOTllZTFlYjA3NmMwOTdjM2JkOTY1NGM2ODZjYjkyZTAyMzk2Y2Q0YjU2MWY1MjY5NjI5ZGQ5NTliYjU0YzEwOGI4OGVmNjdjMTVlZTdjZDc2YTRiMGU5NzkzNzA3YzkxYzFkOWFjNTg0YmQzNjFi",
-//     },
-//     frame: {
-//       version: "1",
-//       name: "Frames v2 Demo",
-//       iconUrl: `${appUrl}/icon.png`,
-//       homeUrl: appUrl,
-//       imageUrl: `${appUrl}/frames/hello/opengraph-image`,
-//       buttonTitle: "Launch Frame",
-//       splashImageUrl: `${appUrl}/splash.png`,
-//       splashBackgroundColor: "#f7f7f7",
-//       webhookUrl: `${appUrl}/api/webhook`,
-//     },
-//   };
-
-//   return Response.json(config);
-// }
+  const config =
 {
   "accountAssociation": {
     header: "eyJmaWQiOjI2ODQzOCwidHlwZSI6ImN1c3RvZHkiLCJrZXkiOiIweDIxODA4RUUzMjBlREY2NGMwMTlBNmJiMEY3RTRiRkIzZDYyRjA2RWMifQ",
-    payload: "eyJkb21haW4iOiJkZWdlbi12Mi52ZXJjZWwuYXBwIn0",
-    signature: "MHhjMzkxYTc4MTRlMWQwNDI5MzVmNGQzYjJlYWVmNWE5YTQwNDVhN2E1YjZiNDlkZjAxMDhjZDFmN2YzYzYwMDg5NDljMDdjYzFhOGYwYjk0MWUzNzBjZDRlMjFkOTU1MGFjMjg4YjdmNDk0MzJjZGNmY2U0MDJkNzc2MmM3ZTI2ZDFj"
+    payload: "eyJkb21haW4iOiJ0ZXN0LWRlZ2VuLnZlcmNlbC5hcHAifQ",
+    signature: "MHhjN2JlMzVmZGUyODhmNmZlMDExNzA5YzAyYjY2MDIxMWMyMWM5OTJiNzMwNzZhZDc3ZWE2OWQxNGE3NmNlZWUwMjFmZDdlZTg5ZGRhMzBkNTE3MmYzYjM0MWYwOGQ5YWYzNDllZTQyOTlkZWFhYzc0YjkyM2I2MWQ5YmI2MTM1MzFj"
   },
   frame: {
     version: "1",
-    name: "DEGEN",
+    name: "TEST",
     iconUrl: "https://raw.githubusercontent.com/cashlessman/images/refs/heads/main/degen_logo.png",
     homeUrl: appUrl,
     imageUrl: "https://raw.githubusercontent.com/cashlessman/images/refs/heads/main/degen.png",
